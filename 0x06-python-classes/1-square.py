f5e60b543dd62882cf2366d6a4481e4f8ae65129
Kevv-commits <knganga28@gmail.com>
2021-09-28 14:45:31 -0700

    fixed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python3
-"""
-Define a class Square
-"""
+"""Square with size"""
 
 
-class Square():
-    '''
-    This class represents a Square.
-    '''
-    def __init__(self, size=0):
-        '''
-        Initialization of instance atributtes
-        Args:
-            size(int): size of square, must be integer and greater than zero
-        '''
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+class Square:
+    """Representation of a square"""
+
+    def __init__(self, size):
+        """Instantiation with size"""
         self.__size = size
-        
