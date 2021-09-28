de545fa6abf49581522d3c3e5615b082a5e03d1b
Kevv-commits <knganga28@gmail.com>
2021-09-28 15:04:32 -0700

    fixed and comp

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,50 +1,38 @@
 #!/usr/bin/python3
-class Square():
-    '''
-        Defining a Square
-    '''
+"""Square with size"""
+
+
+class Square:
+    """Representation of a square"""
 
     def __init__(self, size=0):
-        '''Initialization of instance attributes
-            Args:
-            size (int): Zero or positve number.
-        '''
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-        
-    def area(self):
-        '''Calculates the area
-            Return: The current square area.
-        '''
-        return self.__size * self.__size
+        """Instantiation with optional size"""
+        self.size = size
 
     @property
     def size(self):
-        return self.__size
+        """Property to retrieve size"""
+        return (self.__size)
     
     @size.setter
     def size(self, value):
-        '''Updating the private attributes
-            Args:
-            value (int): Zero or positve number.
-        '''
-        if not isinstance(value, int):
+        """Property setter to set size"""
+        if type(value) != int:
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+        
+    def area(self):
+        """Public instance method that returns the current square area"""
+        return self.__size ** 2
 
-        def my_print(self):
-            '''prints in stdout the square with the character # or a new line
-               is size is zero.
-            '''
-            if self.__size == 0:
-                print()
-                return
-            for col in range(self.__size):
-                for row in range(self.size):
-                    print("#", end="")
-                print()
+    def my_print(self):
+        """Public instance method that prints the square with char #"""
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+                print("")
+                if self.__size == 0:
+                    print("")
+                    
