ea92227c4c81a53f66e6ae0d674fbece3e87cdc1
Kevv-commits <knganga28@gmail.com>
2021-09-28 15:00:43 -0700

    fixed err

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,38 +1,29 @@
 #!/usr/bin/python3
-class Square():
-    '''
-        Defining a Square
-    '''
+"""Square with size"""
 
-    def __init__(self, size=0):
-        '''Initialization of instance attributes
-            Args:
-            size (int): Zero or positve number.
-        '''
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-        
-    def area(self):
-        '''Calculates the area
-            Return: The current square area.
-        '''
-        return self.__size * self.__size
+
+class Square:
+    """Representation of a square"""
     
+    def __init__(self, size=0):
+        """Instantiation with optional size"""
+        self.size = size
+        
     @property
     def size(self):
-        return self.__size
+        """Property to retrieve size"""
+        return (self.__size)
     
     @size.setter
     def size(self, value):
-        '''Updating the private attributes
-            Args:
-            value (int): Zero or positve number.
-        '''
-        if not isinstance(value, int):
+        """Property setter to set size"""
+        if type(value) != int:
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+        
+    def area(self):
+        """Public instance method that returns the current square area"""
+        return self.__size ** 2
+    
