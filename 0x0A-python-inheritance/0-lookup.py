38071fa14a0453afa6738d8b62459ee30879b31d
Kevv-commits <knganga28@gmail.com>
2021-10-11 13:28:11 -0700

    fixed

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Lookup"""
+
+
 def lookup(obj):
-    '''
-        Returns a list of avaiblable attributes and methods in the object
-    '''
+    """returns the list of available attributes and methods of the object"""
+
     return (dir(obj))
+        
