44f1d60ca0f8b3921dfeafc5a30ec2159863532d
Kevv-commits <knganga28@gmail.com>
2021-10-11 13:58:06 -0700

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -10,9 +10,17 @@ class Square(Rectangle):
         Implementing the class Square
     '''
     def __init__(self, size):
+        '''
+        initialize the square class
+        take the init from rectangle class
+        take the method integer validator from baseGeometry
+        '''
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
         
     def area(self):
+        '''
+        define the area to the square
+        '''
         return (self.__size ** 2)
