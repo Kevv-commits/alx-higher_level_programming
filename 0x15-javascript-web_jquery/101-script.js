0bb07aa6e47a9993c0f3515e1a0c062cf8034989
Kevin Nganga <knganga28@gmail.com>
2022-02-10 07:59:02 +0000

    task done

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+  $('div#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+
+  $('div#remove_item').click(function () {
+    $('ul.my_list li:last-child').remove();
+  });
+
+  $('div#clear_list').click(function () {
+    $('ul.my_list li').each(function () {
+      $(this).remove();
+    });
+  });
+});
